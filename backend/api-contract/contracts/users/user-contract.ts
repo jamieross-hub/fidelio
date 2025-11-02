ee3b734b29e0e5385ce960df4af7f202fa07c5cc
theelgreco <stelios.kakoliris@yahoo.com>
2025-11-02 22:18:49 +0000

    feat: add validateJWT endpoint to verify user's JWT

--- backend/api-contract/contracts/users/user-contract.ts
+++ backend/api-contract/contracts/users/user-contract.ts
@@ -1,9 +1,18 @@
 import { initContract } from "@ts-rest/core";
 import { UserSchema } from "../../../prisma/generated/zod";
+import z from "zod";
 
 const client = initContract();
 
 export const userContract = client.router({
+    validateJWT: {
+        method: "GET",
+        path: "/user",
+        summary: "Validate the current user's JWT",
+        responses: {
+            200: z.object({ msg: z.literal("Your JWT is valid") }),
+        },
+    },
     getUser: {
         method: "GET",
         path: "/user",
