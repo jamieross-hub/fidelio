aeca4a4573022b7928be87755b71e65cea2c6603
theelgreco <stelios.kakoliris@yahoo.com>
2025-12-11 00:58:56 +0000

    fix: update commonResponses to use commonErrorType in contract.ts

--- backend/api-contract/contracts/contract.ts
+++ backend/api-contract/contracts/contract.ts
@@ -2,6 +2,7 @@ import z from "zod";
 import { userContract } from "./users/user-contract";
 import { transactionContract } from "./transactions/transaction-contract";
 import { initContract } from "@ts-rest/core";
+import { commonErrorType } from "../errors";
 
 const client = initContract();
 
@@ -16,8 +17,10 @@ export const contract = client.router(
         },
         pathPrefix: "/api",
         commonResponses: {
-            404: z.object({ message: z.string() }),
-            500: z.object({ message: z.string() }),
+            400: commonErrorType,
+            401: commonErrorType,
+            403: commonErrorType,
+            500: commonErrorType,
         },
     }
 );
