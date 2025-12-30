4c58a0819a01894c75a1e7cdfd204323a7079c86
theelgreco <stelios.kakoliris@yahoo.com>
2025-12-30 13:18:09 +0000

    chore: removes storybook

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,15 +6,10 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import { VitePWA } from "vite-plugin-pwa";
 import Components from "unplugin-vue-components/vite";
 
-// https://vitejs.dev/config/
-import path from "node:path";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-
 const contractsPath = fileURLToPath(new URL(process.env.API_CONTRACT_PATH || "../backend/api-contract", import.meta.url));
 const schemaPath = fileURLToPath(new URL(process.env.SCHEMA_PATH || "../backend/prisma/zod/schemas/variants", import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
@@ -62,33 +57,5 @@ export default defineConfig({
             "@schemas": schemaPath,
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-    },
-    test: {
-        projects: [
-            {
-                extends: true,
-                plugins: [
-                    // The plugin will run tests for the stories defined in your Storybook config
-                    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-                    storybookTest({
-                        configDir: path.join(dirname, ".storybook"),
-                    }),
-                ],
-                test: {
-                    name: "storybook",
-                    browser: {
-                        enabled: true,
-                        headless: true,
-                        provider: "playwright",
-                        instances: [
-                            {
-                                browser: "chromium",
-                            },
-                        ],
-                    },
-                    setupFiles: [".storybook/vitest.setup.ts"],
-                },
-            },
-        ],
     },
 });
