f67016ebb7ba73f905441f67f5f0b703f6b8e1a0
theelgreco <stelios.kakoliris@yahoo.com>
2026-01-24 03:08:37 +0000

    fix: adjust padding and update drawer component properties for better layout and functionality

--- frontend/src/components/navigation/NavigationSidebarToggle.vue
+++ frontend/src/components/navigation/NavigationSidebarToggle.vue
@@ -1,33 +1,48 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { DrawerHandle, DrawerRoot, DrawerTrigger, DrawerPortal, DrawerOverlay, DrawerContent } from "vaul-vue";
-
-const isOpen = ref(true);
+import { computed, ref } from "vue";
+import { DrawerHandle, DrawerRoot, DrawerPortal, DrawerContent } from "vaul-vue";
+import { clamp } from "lodash";
 
 const snapPoints = [`${window.innerHeight - 215}px`, 1];
 const activeSnapPoint = ref(snapPoints[0]);
 
+const isOpen = computed(() => activeSnapPoint.value === snapPoints[1]);
+
+const dragPoint = ref(1);
+
+const overlayOpacity = computed(() => clamp(dragPoint.value, 0, 0.5));
+
 const setSnap = (snap: number | string) => {
-    if (typeof snap === "number") activeSnapPoint.value = snap;
+    activeSnapPoint.value = snap;
+    dragPoint.value = clamp(snapPoints.indexOf(snap), 0, 0.5);
 };
+
+const handleDrag = (value: number) => (dragPoint.value = (1 - value) / 2);
 </script>
 
 <template>
     <DrawerRoot
         direction="bottom"
-        :open="isOpen"
+        :open="true"
         :handleOnly="true"
         :dismissible="false"
-        :snap-points="snapPoints"
-        :active-snap-point="activeSnapPoint"
+        :snapPoints="snapPoints"
+        :activeSnapPoint="activeSnapPoint"
         :modal="false"
+        :should-scale-background="true"
         @update:active-snap-point="setSnap"
+        @drag="handleDrag"
     >
         <DrawerPortal>
+            <div
+                @click="setSnap(snapPoints[0])"
+                class="md:hidden fixed top-0 left-0 w-full h-full bg-black z-9998"
+                :style="{ opacity: overlayOpacity, pointerEvents: isOpen ? 'auto' : 'none' }"
+            ></div>
             <DrawerContent
-                class="flex flex-col gap-5 z-9999 h-full mt-24 max-h-fit fixed bottom-0 left-0 right-0 bg-card p-4 menu-shadow rounded-t-2xl"
+                class="md:hidden flex flex-col gap-5 z-9999 h-full mt-24 max-h-fit fixed bottom-0 left-0 right-0 bg-card p-4 menu-shadow rounded-t-2xl"
             >
-                <DrawerHandle class="bg-accent! w-24!"></DrawerHandle>
+                <DrawerHandle class="bg-accent! w-24!" />
                 <div>
                     <p>Content</p>
                     <p>Content</p>
